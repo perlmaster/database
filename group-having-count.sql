d50f72f2a43151d618a0382beb95c5aff33af2be
Barry Kimelman <36140641+perlmaster@users.noreply.github.com>
2019-05-14 12:44:43 -0600

    Update group-having-count.sql

--- group-having-count.sql
+++ group-having-count.sql
@@ -4,13 +4,6 @@ group by ip_address_1
 having count(*) > 1
 order by IP1;
 
-
-select NSS_ID+'.'+ProjectID as stuff,count(*) mycount
-from Contacts
-group by NSS_ID+'.'+ProjectID
-order by mycount desc;
-
-
 select NSS+'.'+ProjectID as stuff,count(*) mycount
 from CPA
 group by NSS+'.'+ProjectID
@@ -21,9 +14,8 @@ AVG(BillingTotal) AS BillingAvg
 FROM Billings JOIN Bankers
 ON Billings.BankerID = Bankers.BankerID
 GROUP BY BankerState, BankerCity
-ORDER BY BankerState, BankerCity
+ORDER BY BankerState, BankerCity;
 
-go
-
-select purchase_date, item, sum(items_purchased) as 
-"Total Items" from Purchases group by item, purchase_date;
+select purchase_date, item, sum(items_purchased) as "Total Items"
+from Purchases
+group by item, purchase_date;
